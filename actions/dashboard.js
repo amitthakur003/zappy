f507bb3886bbcc662d655f2cee5b9bd958a04e8a
amitthakur003 <reachthakuramit@gmail.com>
2025-03-01 23:49:35 +0530

    Update

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -25,7 +25,6 @@ export async function getIndustryInsights() {
           keyTrends: [],
           recommendedSkills: [],
           nextUpdate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-console.log("hii amt jii");
         }
       })
     }
