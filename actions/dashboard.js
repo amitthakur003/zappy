1450aa75b37a7a0f6d1e095aca51dcc1a78ffe80
amitthakur003 <reachthakuramit@gmail.com>
2025-03-04 00:08:55 +0530

    Update

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -1,9 +1,47 @@
 "use server"
-import { getIndustryInsights } from '@/actions/dashboard';
+
 import { db } from '@/lib/prisma';
 import { auth } from '@clerk/nextjs/server';
+import { getIndustryInsight } from '@/actions/dashboard';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import { Content } from './../node_modules/@google/generative-ai/dist/server/types/content.d';
+import { NextResponse } from 'next/server';
 
-export async function getIndustryInsights() {
+const genAI = new GoogleGenerativeAI(process.env.GEMINI.API.KEY);
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash",
+})
+export const generateAIInsights = async (industry) => {
+  const prompt = `
+  Analyze the current state of the ${industry} industry and provide insights in ONLY the following JSON format without any additional notes or explanations:
+  {
+    "salaryRanges": [
+      { "role": "string", "min": number, "max": number, "median": number, "location": "string" }
+    ],
+    "growthRate": number,
+    "demandLevel": "High" | "Medium" | "Low",
+    "topSkills": ["skill1", "skill2"],
+    "marketOutlook": "Positive" | "Neutral" | "Negative",
+    "keyTrends": ["trend1", "trend2"],
+    "recommendedSkills": ["skill1", "skill2"]
+  }
+  
+  IMPORTANT: Return ONLY the JSON. No additional text, notes, or markdown formatting.
+  Include at least 5 common roles for salary ranges.
+  Growth rate should be a percentage.
+  Include at least 5 skills and trends.
+`;
+
+const result = await model.generate.Content(prompt)
+const response = result.response;
+const text = response.text()
+
+const cleanedText = text.replace(/```(?:json)?\n?/g, "").trim();
+return JSON.parse(cleanedText);
+
+};
+
+export async function  getIndustryInsights() {
     const { userId } = await auth();
     if (!userId) throw new Error("Unauthorized");
   
@@ -13,20 +51,19 @@ export async function getIndustryInsights() {
   
     if (!user) throw new Error("User not found");
 
-    if (!industryInsight) {
-      industryInsight = await tx.industryInsight.create({
-        data: {
-          industry: data.industry,
-          dalaryRanges: [],
-          growthRate: 0,
-          demandLevel: "MEDIUM" ,
-          topSkills: [],
-          marketOutLook : "NEUTRAL",
-          keyTrends: [],
-          recommendedSkills: [],
-          nextUpdate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    if(!user.industryInsight){
+      const insights = await generateAIInsights(user.industry);
 
+      const getIndustryInsight = await db.industryInsight.create({
+        data:{
+          industry: user.industry,
+          ...insights,
+          nextUpdate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
         }
       })
+
+      return industryInsight;
     }
-}
+
+    return user.industryInsight;
+}  
