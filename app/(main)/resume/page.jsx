136a46736213209e234dac88f2c85e54ba4731e4
amitthakur003 <reachthakuramit@gmail.com>
2025-03-06 22:06:59 +0530

    Update

---
+++ app/(main)/resume/page.jsx
@@ -0,0 +1,14 @@
+import { getResume } from '@/actions/resume';
+import ResumeBuilder from './_components/resume-builder';
+
+const ResumePage = async () => {
+const resume = await getResume();
+
+  return (
+    <div>
+      <ResumeBuilder initialContent= {resume?.content} />
+    </div>
+  )
+}
+
+export default ResumePage
