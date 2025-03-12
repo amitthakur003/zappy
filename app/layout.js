7144198a2a5006318e91177f8811839723a1799d
amitthakur003 <reachthakuramit@gmail.com>
2025-03-12 23:52:19 +0530

    Updated

--- app/layout.js
+++ app/layout.js
@@ -41,7 +41,7 @@ export default function RootLayout({ children }) {
             {/* footer */}
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200 ">
-                <p> Made by Amit Thakur yes no</p>
+                <p> Made by Amit Thakur yes no yw</p>
               </div>
             </footer>
            
